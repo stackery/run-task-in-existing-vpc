3a3d99ee5a9b056e86a47ac84f2bc186679550ba
Sam Goldstein <sam@stackery.io>
2018-12-11 11:47:55 -0800

    Function code

--- src/runReport/index.js
+++ src/runReport/index.js
@@ -1,5 +1,46 @@
+const AWS = require('aws-sdk');
+const mysql = require('mysql');
+
 exports.handler = async message => {
   console.log(message);
 
-  return {};
+  const connection = mysql.createConnection({
+    host: process.env.DB_ADDRESS,
+    user: 'root',
+    password: await getDBPassword(),
+    database: 'items'
+  });
+
+  const rows = await new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM items', (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+
+  connection.destroy();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(rows),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+};
+
+let _dbPassword;
+async function getDBPassword () {
+  if (!_dbPassword) {
+    const secretsManager = new AWS.SecretsManager();
+
+    ({ SecretString: _dbPassword } = await secretsManager.getSecretValue({
+      SecretId: process.env.SECRETS_NAMESPACE + 'dbPassword'
+    }).promise());
+  }
+
+  return _dbPassword;
 }
